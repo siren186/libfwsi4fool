9b8621f0b9dd3ec694a1f545e1848338c95856fa
siren186 <siren186@163.com>
2022-08-18 09:46:26 +0800

    执行完脚本后的代码入库 git clone https://github.com/libyal/libfwsi.git cd libfwsi\ .\synclibs.ps1 .\autogen.ps1

---
+++ libfwsi-official/libfole/libfole_types.h
@@ -0,0 +1,47 @@
+/*
+ * The internal type definitions
+ *
+ * Copyright (C) 2008-2022, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#if !defined( _LIBFOLE_INTERNAL_TYPES_H )
+#define _LIBFOLE_INTERNAL_TYPES_H
+
+#include <common.h>
+#include <types.h>
+
+/* Define HAVE_LOCAL_LIBFOLE for local use of libfole
+ * The definitions in <libfole/types.h> are copied here
+ * for local use of libfole
+ */
+#if defined( HAVE_LOCAL_LIBFOLE )
+
+/* The following type definitions hide internal data structures
+ */
+#if defined( HAVE_DEBUG_OUTPUT ) && !defined( WINAPI )
+
+
+#else
+
+
+#endif /* defined( HAVE_DEBUG_OUTPUT ) && !defined( WINAPI ) */
+
+#endif /* defined( HAVE_LOCAL_LIBFOLE ) */
+
+#endif /* !defined( _LIBFOLE_INTERNAL_TYPES_H ) */
+
